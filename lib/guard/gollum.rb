b025a5689b7aa583fb26a4368da89e134fb327f9
Xavier Tô <xavier.to.01@gmail.com>
2012-05-29 12:57:58 -0400

    Kill gollum silently

--- lib/guard/gollum.rb
+++ lib/guard/gollum.rb
@@ -3,21 +3,20 @@ require 'guard/guard'
 
 module Guard
   class Gollum < Guard
-    attr_accessor :pid
     def initialize(watchers = [], options = {})
       super
     end
 
     def start 
       puts "Wake up. Wake up. Wake up, sleepies. We must go, yeeees, we must go at once."
-      `gollum`
-      pid = $?.pid
+      @pid= fork {exec "gollum > /dev/null 2>1"}
+      Process.detach(@pid)
       puts "My Precious!!!!"
     end
 
     def stop
-      `kill -9 #{pid}`
-      puts "Oh! Cruel hobbit! It does not care if we be hungry. It does not care if we should die! Not like Master. Master cares. Master knows. Yes, precious..."
+      Process.kill("INT", @pid)
+      puts "Oh! Cruel hobbit!"
     end
   end
 end
